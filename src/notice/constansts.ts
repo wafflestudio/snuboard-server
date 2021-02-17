6431b521b2b80564ed6f60c35e4dfb7076360f98
sangmin <min112402@gmail.com>
2021-02-17 21:42:10 +0900

    fix notice.init

--- src/notice/constansts.ts
+++ src/notice/constansts.ts
@@ -1,16 +1,14 @@
 export const NOTICES = [
   {
-    id: 1,
     title: '2021-2022 러시아 정부초청장학생 선발 모집 안내',
     preview:
       '학생 여러분께,\n\n2021-2022 러시아 정부초청장학생 선발 모집을 아래와 같이 안내합니다.\n\n가. ',
     content:
       '<p><span style="font-family: Arial;">&#xD559;&#xC0DD; &#xC5EC;&#xB7EC;&#xBD84;&#xAED8;,</span></p>\n<p><span style="font-family: ',
     createdAt: '2021-02-08 14:45:14',
-    isPinned: false,
+    isPinned: true,
     link: 'https://cse.snu.ac.kr/node/47407',
-    cursor: 1612763114001,
-    department: 1,
+    department: '컴퓨터공학부',
     files: [],
     noticeTags: [
       '학사 (학부)',
@@ -20,7 +18,6 @@ export const NOTICES = [
     ],
   },
   {
-    id: 2,
     title:
       '2021학년도 1학기 군휴학생 원격수업 수강신청 안내/ 학점인정 신청 안내',
     preview:
@@ -30,13 +27,10 @@ export const NOTICES = [
     createdAt: '2021-02-05 18:56:00',
     isPinned: true,
     link: 'https://cse.snu.ac.kr/node/47396',
-    cursor: 1612518960002,
-    department: 1,
-    files: [],
+    department: '컴퓨터공학부',
     noticeTags: ['수업', '학사 (학부)', '학사 (대학원)'],
   },
   {
-    id: 3,
     title:
       "2021학년도 1학기 컴퓨터공학부 개설 교과목 '정원 외 신청(구: 초안지)' 안내",
     preview:
@@ -46,13 +40,10 @@ export const NOTICES = [
     createdAt: '2021-02-02 07:32:14',
     isPinned: true,
     link: 'https://cse.snu.ac.kr/node/47377',
-    cursor: 1612218734003,
-    department: 1,
-    files: [],
+    department: '컴퓨터공학부',
     noticeTags: ['수업', '학사 (학부)', '학사 (대학원)', '제2전공/전과'],
   },
   {
-    id: 4,
     title: '2021학년도 1학기 안전환경 신규교육 안내',
     preview:
       '2021년 1학기 안전환경 신규교육에 대한 안내 입니다. *코로나로 인하여 온라인 교육으로 진행',
@@ -61,22 +52,17 @@ export const NOTICES = [
     createdAt: '2021-02-08 15:31:53',
     isPinned: false,
     link: 'https://me.snu.ac.kr/ko/board/notice/view/13639',
-    cursor: 1612765913004,
-    department: 2,
-    files: [],
+    department: '기계공학부',
     noticeTags: ['대학원'],
   },
   {
-    id: 5,
     title: '[학사] 학칙 개정에 따른 학사과정 소속 변경 신청 안내',
     preview: '안녕하세요. 기계공학전공 행정실입니다.',
     content: '<p>안녕하세요. 기계공학전공 행정실입니다.</p>',
     createdAt: '2020-12-01 09:48:11',
     isPinned: true,
     link: 'https://me.snu.ac.kr/ko/board/notice/view/13367',
-    cursor: 1606783691005,
-    department: 2,
-    files: [],
+    department: '기계공학부',
     noticeTags: ['학부'],
   },
 ];
@@ -86,36 +72,39 @@ export const FILES = [
     name: '2021-2022 Russian Government Scholarships (ENG).pdf',
     link:
       'https://cse.snu.ac.kr/sites/default/files/node--notice/2021-2022%20Russian%20Government%20Scholarships%20%28ENG%29.pdf',
-    noticeId: 1,
+    noticeTitle: '2021-2022 러시아 정부초청장학생 선발 모집 안내',
   },
   {
     name: '2021-2022 러시아 정부초청 장학생 선발안내(국문).hwp',
     link:
       'https://cse.snu.ac.kr/sites/default/files/node--notice/2021-2022%20%EB%9F%AC%EC%8B%9C%EC%95%84%20%EC%A0%95%EB%B6%80%EC%B4%88%EC%B2%AD%20%EC%9E%A5%ED%95%99%EC%83%9D%20%EC%84%A0%EB%B0%9C%EC%95%88%EB%82%B4%28%EA%B5%AD%EB%AC%B8%29.hwp',
-    noticeId: 1,
+    noticeTitle: '2021-2022 러시아 정부초청장학생 선발 모집 안내',
   },
   {
     name: 'Application form(Russian government scholarships).docx',
     link:
       'https://cse.snu.ac.kr/sites/default/files/node--notice/Application%20form%28Russian%20government%20scholarships%29.docx',
-    noticeId: 1,
+    noticeTitle: '2021-2022 러시아 정부초청장학생 선발 모집 안내',
   },
   {
     name: '2021학년도 1학기 군휴학생 원격수업 수강신청 안내.pdf',
     link:
       'https://cse.snu.ac.kr/sites/default/files/node--notice/2021%ED%95%99%EB%85%84%EB%8F%84%201%ED%95%99%EA%B8%B0%20%EA%B5%B0%ED%9C%B4%ED%95%99%EC%83%9D%20%EC%9B%90%EA%B2%A9%EC%88%98%EC%97%85%20%EC%88%98%EA%B0%95%EC%8B%A0%EC%B2%AD%20%EC%95%88%EB%82%B4.pdf',
-    noticeId: 2,
+    noticeTitle:
+      '2021학년도 1학기 군휴학생 원격수업 수강신청 안내/ 학점인정 신청 안내',
   },
   {
     name: '(국문) 2021년 1학기 정원외신청 매뉴얼_학생.pdf',
     link:
       'https://cse.snu.ac.kr/sites/default/files/node--notice/%28%EA%B5%AD%EB%AC%B8%29%202021%EB%85%84%201%ED%95%99%EA%B8%B0%20%EC%A0%95%EC%9B%90%EC%99%B8%EC%8B%A0%EC%B2%AD%20%EB%A7%A4%EB%89%B4%EC%96%BC_%ED%95%99%EC%83%9D.pdf',
-    noticeId: 3,
+    noticeTitle:
+      "2021학년도 1학기 컴퓨터공학부 개설 교과목 '정원 외 신청(구: 초안지)' 안내",
   },
   {
     name: '(영문) 2021년 1학기 정원외신청 매뉴얼_학생.pdf',
     link:
       'https://cse.snu.ac.kr/sites/default/files/node--notice/%28%EC%98%81%EB%AC%B8%29%202021%EB%85%84%201%ED%95%99%EA%B8%B0%20%EC%A0%95%EC%9B%90%EC%99%B8%EC%8B%A0%EC%B2%AD%20%EB%A7%A4%EB%89%B4%EC%96%BC_%ED%95%99%EC%83%9D.pdf',
-    noticeId: 3,
+    noticeTitle:
+      "2021학년도 1학기 컴퓨터공학부 개설 교과목 '정원 외 신청(구: 초안지)' 안내",
   },
 ];
--- src/notice/notice.init.ts
+++ src/notice/notice.init.ts
@@ -1,40 +1,33 @@
+import { Logger } from '@nestjs/common';
 import { Department, NoticeTag, Tag } from '../department/department.entity';
 import { NOTICES, FILES } from './constansts';
 import { Notice, File } from './notice.entity';
 export const noticeInit = async (): Promise<void> => {
   for (const noticeData of NOTICES) {
-    const {
-      id,
-      title,
-      preview,
-      content,
-      createdAt,
-      isPinned,
-      link,
-      cursor,
-      department,
-      noticeTags,
-    } = noticeData;
-
-    let notice: Notice | undefined = await Notice.findOne(id);
+    let notice: Notice | undefined = await Notice.findOne({
+      title: noticeData.title,
+    });
     if (!notice) {
-      const departmentObj = await Department.findOne(department);
-      //const tags = await Promise.all(noticeTags.map((tagName)=> Tag.findOne({name:tagName,department:departmentObj})));
+      const departmentObj: Department | undefined = await Department.findOne({
+        name: noticeData.department,
+      });
       if (departmentObj) {
         notice = Notice.create({
-          id,
-          title,
-          preview,
-          content,
-          isPinned,
-          link,
-          cursor: cursor,
+          title: noticeData.title,
+          preview: noticeData.preview,
+          content: noticeData.content,
+          createdAt: new Date(noticeData.createdAt),
+          department: departmentObj,
+          isPinned: noticeData.isPinned,
+          link: noticeData.link,
         });
-        notice.createdAt = new Date(createdAt);
-        notice.department = departmentObj;
+        Logger.log(notice);
+        notice.cursor = notice.createdAt.getTime();
+        await Notice.save(notice); // save notice to generate id
+        notice.cursor = notice.createdAt.getTime() + (notice.id % 1000);
         await Notice.save(notice);
       }
-      noticeTags.map(async (tagName) => {
+      noticeData.noticeTags.map(async (tagName) => {
         const tag = await Tag.findOne({
           name: tagName,
           department: departmentObj,
@@ -45,8 +38,8 @@ export const noticeInit = async (): Promise<void> => {
   }
 
   for (const fileData of FILES) {
-    const { name, link, noticeId } = fileData;
-    const notice = await Notice.findOne(noticeId);
+    const { name, link, noticeTitle } = fileData;
+    const notice = await Notice.findOne({ title: noticeTitle });
     const file = await File.findOne({ name: name, notice: notice });
     if (!file) {
       File.save(File.create({ name: name, link: link, notice: notice }));
