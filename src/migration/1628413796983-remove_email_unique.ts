297edc44869045bf5e2698e8e6d5231090c5ee92
eldpswp99 <nrg1392@naver.com>
2021-08-08 18:58:51 +0900

    migration sync

--- src/migration/1628413796983-remove_email_unique.ts
+++ src/migration/1628413796983-remove_email_unique.ts
@@ -2,6 +2,9 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class removeEmailUnique1628413796983 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      'alter table user drop index IDX_e2364281027b926b879fa2fa1e',
+    );
     await queryRunner.query('alter table user drop index email');
   }
 
