10c37f3c82323a132c9ade61a6b766f62c8d31e8
eldpswp99 <nrg1392@naver.com>
2021-08-11 00:11:27 +0900

    update with migration

--- src/migration/1628599386012-revert tagIds and add noticeTag notice CreateAt.ts
+++ src/migration/1628599386012-revert tagIds and add noticeTag notice CreateAt.ts
@@ -10,6 +10,9 @@ export class revertTagIdsAndAddNoticeTagNoticeCreateAt1628599386012
       "alter table notice_tag add `noticeCreatedAt` timestamp NOT NULL default '1970-01-01 09:00:01'",
     );
     await queryRunner.query(
+      'update notice_tag, notice set notice_tag.noticeCreatedAt = notice.createdAt where notice_tag.noticeId = notice.id;',
+    );
+    await queryRunner.query(
       'alter table notice_tag add index `notice_cursor` (`noticeCreatedAt`, `noticeId`)',
     );
   }
